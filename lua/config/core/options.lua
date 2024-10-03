0d78e22a102a4c5cd96b4b6d2ce9089e63c98bd8
Antr0z <mr.antroz@gmail.com>
2024-10-03 23:05:09 +0200

    added options in core

---
+++ lua/config/core/options.lua
@@ -0,0 +1,11 @@
+local opt = vim.opt
+
+opt.number = true
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.autoindent = true
+
+opt.ignorecase = true
+opt.smartcase = true
+opt.termguicolors = true
+opt.signcolumn = "yes"
